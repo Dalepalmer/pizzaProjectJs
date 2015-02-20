769ddd21a1594530f797e9550cc909a6c22f0bc1
Dale <student@epicodus.com>
2015-02-20 11:15:42 -0800

    input user defined fields, calculate how many cuts their pizza has, and toppings

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,17 @@
-var Pizza = {};
+var Pizza = {
+  cut: 0,
+
+  pizzaCut: function() {
+    this.cut = this.diameter / 3.14;
+    return Math.ceil(this.cut);
+  }
+};
+
+var Topping = {
+  name: "",
+}
+
+var toppings = []
 
 
 $(document).ready(function() {
@@ -7,8 +20,20 @@ $(document).ready(function() {
     event.preventDefault();
 
     var inputtedDiameter = $("input#pizza-size").val();
+    var inputtedName = $("input#customer").val();
+    var inputtedTopping = $("input#topping").val();
+
 
     var newPizza = Object.create(Pizza);
+    newPizza.customerName = inputtedName;
+    newPizza.diameter = inputtedDiameter;
+    newPizza.toppings = inputtedTopping;
+    newPizzaCut = newPizza.pizzaCut();
+
+    $("ul#customer").append("<p>" + "Customer Name:" + " " + newPizza.customerName + "</p>");
+    $("ul#toppings").append("<p>" + "Toppings:" + " " + newPizza.toppings + "</p>");
+    $("ul#pizza-output").append("<p>" + "Amount of slices:" + " " + newPizzaCut + "</p>");
+
   })
 
 
--- spec/specs.js
+++ spec/specs.js
@@ -1,10 +1,24 @@
 describe("Pizza", function() {
-
   it("creates a Pizza object", function() {
     var testPizza = Object.create(Pizza);
     testPizza.customerName = "Renato";
-    testPizza.toppings = "cheese"
-    testPizza.diameter = 12
+    testPizza.toppings = "cheese";
+    testPizza.diameter = 12;
     expect(testPizza.customerName).to.equal("Renato");
-  })
-})
+  });
+  it("cuts the pizza into slices", function() {
+    var testPizza = Object.create(Pizza);
+    testPizza.customerName = "Stephanie";
+    testPizza.toppings = "cheese";
+    testPizza.diameter = 12;
+    expect(testPizza.pizzaCut()).to.equal(4);
+  });
+});
+
+describe("Topping", function() {
+  it("create a Topping object", function() {
+    var testTopping = Object.create(Topping);
+    testTopping.name = "Cheese";
+    expect(testTopping.name).to.equal("Cheese");
+  });
+});
