b3d38b619d627c3861732a62364e6f37ac50aaa0
Dale <student@epicodus.com>
2015-02-20 13:52:25 -0800

    take users input, calculate their pizza cut, and give it back to them

--- js/scripts.js
+++ js/scripts.js
@@ -2,36 +2,52 @@ var Pizza = {
   cut: 0,
 
   pizzaCut: function() {
-    this.cut = this.diameter / 3.14;
-    return Math.ceil(this.cut);
+    if (this.topping === "Pepperoni") {
+      this.cut = this.diameter * 4 ;
+      return 2 * Math.floor(this.cut / 2);
+    } else {
+      this.cut = this.diameter / 3.14;
+      return 2 * Math.ceil(this.cut / 2);
+    }
   }
 };
 
-var Topping = {
-  name: "",
-}
-
-var toppings = []
+// Was going to make a topping prototype, but with two toppings it seemed like overkill
+// var Topping = {
+//   name: "",
+// }
 
 
 $(document).ready(function() {
+  $(".jumbotron#pizza").hide();
+  $("ul#customer").hide();
+  $("ul#toppings").hide();
+  $("ul#pizza-output").hide();
+
 
   $("form#pizza-form").submit(function(event) {
     event.preventDefault();
+    $("div#pizza").show("slide", 630);
+    $("ul#customer").show(700);
+    $("ul#toppings").show(750);
+    $("ul#pizza-output").show(770);
+
 
     var inputtedDiameter = $("input#pizza-size").val();
     var inputtedName = $("input#customer").val();
-    var inputtedTopping = $("input#topping").val();
+    var inputtedTopping = $("select#topping").val();
 
 
     var newPizza = Object.create(Pizza);
     newPizza.customerName = inputtedName;
     newPizza.diameter = inputtedDiameter;
-    newPizza.toppings = inputtedTopping;
+    newPizza.topping = inputtedTopping;
     newPizzaCut = newPizza.pizzaCut();
+    debugger
+
 
     $("ul#customer").append("<p>" + "Customer Name:" + " " + newPizza.customerName + "</p>");
-    $("ul#toppings").append("<p>" + "Toppings:" + " " + newPizza.toppings + "</p>");
+    $("ul#toppings").append("<p>" + "Toppings:" + " " + newPizza.topping + "</p>");
     $("ul#pizza-output").append("<p>" + "Amount of slices:" + " " + newPizzaCut + "</p>");
 
   })
--- spec/specs.js
+++ spec/specs.js
@@ -2,23 +2,25 @@ describe("Pizza", function() {
   it("creates a Pizza object", function() {
     var testPizza = Object.create(Pizza);
     testPizza.customerName = "Renato";
-    testPizza.toppings = "cheese";
     testPizza.diameter = 12;
     expect(testPizza.customerName).to.equal("Renato");
   });
-  it("cuts the pizza into slices", function() {
-    var testPizza = Object.create(Pizza);
-    testPizza.customerName = "Stephanie";
-    testPizza.toppings = "cheese";
-    testPizza.diameter = 12;
-    expect(testPizza.pizzaCut()).to.equal(4);
-  });
-});
-
-describe("Topping", function() {
-  it("create a Topping object", function() {
-    var testTopping = Object.create(Topping);
-    testTopping.name = "Cheese";
-    expect(testTopping.name).to.equal("Cheese");
-  });
+  describe("pizzaCut", function() {
+      it("cuts the pizza into slices", function() {
+      var testPizza = Object.create(Pizza);
+      testPizza.diameter = 12;
+      expect(testPizza.pizzaCut()).to.equal(4);
+    });
+      it("the cuts of pie is rounded to the nearest even number of cuts", function() {
+        var testPizza = Object.create(Pizza);
+        testPizza.diameter = 20;
+        expect(testPizza.pizzaCut()).to.equal(8);
+      });
+      it("when pepperoni is given as a topping the pie is cut differently", function() {
+        var testPizza = Object.create(Pizza);
+        testPizza.topping = "Pepperoni";
+        testPizza.diameter = 20;
+        expect(testPizza.pizzaCut()).to.equal(80)
+      })
+   });
 });
