5780fcbab2f6b980199e18d20bcfd8f1b3da66d7
Dale <student@epicodus.com>
2015-02-20 15:06:20 -0800

    fixed indenting and minor issues

--- js/scripts.js
+++ js/scripts.js
@@ -12,12 +12,6 @@ var Pizza = {
   }
 };
 
-// Was going to make a topping prototype, but with two toppings it seemed like overkill
-// var Topping = {
-//   name: "",
-// }
-
-
 $(document).ready(function() {
   $(".jumbotron#form").hide();
   $(".jumbotron#form").show("slide", 1200);
@@ -34,11 +28,9 @@ $(document).ready(function() {
     $("ul#toppings").show(750);
     $("ul#pizza-output").show(770);
 
-
     var inputtedDiameter = $("input#pizza-size").val();
     var inputtedName = $("input#customer").val();
     var inputtedTopping = $("select#topping").val();
-
 
     var newPizza = Object.create(Pizza);
     newPizza.customerName = inputtedName;
@@ -46,12 +38,8 @@ $(document).ready(function() {
     newPizza.topping = inputtedTopping;
     newPizzaCut = newPizza.pizzaCut();
 
-
     $("ul#customer").append("<p>" + "Customer Name:" + " " + newPizza.customerName + "</p>");
     $("ul#toppings").append("<p>" + "Toppings:" + " " + newPizza.topping + "</p>");
     $("ul#pizza-output").append("<p>" + "Amount of slices:" + " " + newPizzaCut + "</p>");
-
-  })
-
-
-})
+  });
+});
--- spec/specs.js
+++ spec/specs.js
@@ -6,21 +6,21 @@ describe("Pizza", function() {
     expect(testPizza.customerName).to.equal("Renato");
   });
   describe("pizzaCut", function() {
-      it("cuts the pizza into slices", function() {
+    it("cuts the pizza into slices", function() {
       var testPizza = Object.create(Pizza);
       testPizza.diameter = 12;
       expect(testPizza.pizzaCut()).to.equal(4);
     });
-      it("the cuts of pie is rounded to the nearest even number of cuts", function() {
-        var testPizza = Object.create(Pizza);
-        testPizza.diameter = 20;
-        expect(testPizza.pizzaCut()).to.equal(8);
-      });
-      it("when pepperoni is given as a topping the pie is cut differently", function() {
-        var testPizza = Object.create(Pizza);
-        testPizza.topping = "Pepperoni";
-        testPizza.diameter = 20;
-        expect(testPizza.pizzaCut()).to.equal(80)
-      })
-   });
+    it("the cuts of pie is rounded to the nearest even number of cuts", function() {
+      var testPizza = Object.create(Pizza);
+      testPizza.diameter = 20;
+      expect(testPizza.pizzaCut()).to.equal(8);
+    });
+    it("when pepperoni is given as a topping the pie is cut differently", function() {
+      var testPizza = Object.create(Pizza);
+      testPizza.topping = "Pepperoni";
+      testPizza.diameter = 20;
+      expect(testPizza.pizzaCut()).to.equal(80)
+    })
+  });
 });
